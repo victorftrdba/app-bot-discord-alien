0fd3b3aac03371e0cfea352e70ab6934e5739ceb
Cristopher Rissardi <93612872+ALIENxp@users.noreply.github.com>
2022-01-03 23:19:37 -0300

    Update main.py

--- main.py
+++ main.py
@@ -549,8 +549,8 @@ async def ping(ctx):
         colour=29695
     )
 
-    embed.add_field(name='👤 PING DO USUÁRIO', value=f"{round(client.latency * 500)} ms", inline=True)
-    embed.add_field(name='🖥️ PING DO SERVIDOR ', value=f"{round(client.latency * 1000)} ms", inline=True)
+    embed.add_field(name='➢ Ping do usuário', value=f"{round(client.latency * 500)} ms", inline=False)
+    embed.add_field(name='➢ Ping do servidor', value=f"{round(client.latency * 1000)} ms", inline=False)
 
     embed.set_author(name='ㅤㅤㅤCONSULTA DE PINGㅤㅤㅤㅤ', icon_url='https://cdn-icons-png.flaticon.com/512/2695/2695903.png')
 
